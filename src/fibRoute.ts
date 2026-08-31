be555799cc118b4074e86aceb703f575d65ddfdf
belamigw <gwbelami123@gmail.com>
2026-08-31 09:07:27 +0300

    fix (fibRoute.ts): proper types

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,16 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import { Request, Response } from "express";
 
-export default (req, res) => {
+// fib.ts still uses `module.exports`, so it is not an ES module and has to be
+// required. The cast gives the otherwise untyped export a checked signature.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fibonacci = require("./fib") as (n: number) => number;
+
+export default (req: Request, res: Response): void => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
+  const fibN = fibonacci(parseInt(num, 10));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
